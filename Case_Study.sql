2ad25d721da44a598f7044ec732e1a6e15920f70
Mahesh <lmahesh2211@gmail.com>
2026-05-16 20:36:47 +0530

    Add files via upload

---
+++ Case_Study.sql
@@ -0,0 +1,17 @@
+create database Case_Study;
+
+show databases;
+
+use Case_Study;
+
+show tables;
+
+select * from customers;
+
+select * from order_items;
+
+select * from orders;
+
+select * from products;
+
+select * from shipping;
